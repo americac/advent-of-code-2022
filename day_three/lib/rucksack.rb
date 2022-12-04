6728b4022abb6a2258566a2a2b920c2664003416
America Calleros <america.quintero@gmail.com>
2022-12-03 21:29:44 -0600

    Add priorties and specs

--- day_three/lib/rucksack.rb
+++ day_three/lib/rucksack.rb
@@ -1,5 +1,5 @@
 class Rucksack
-  attr_accessor :items, :item_count, :first_compartment, :second_compartment
+  attr_accessor :items, :item_count, :first_compartment, :second_compartment, :all_item_types
 
   def initialize(items)
     @items = items.split('')
@@ -15,5 +15,17 @@ class Rucksack
 
   def item_count
     @item_count ||= items.count
+  end
+
+  def item_priority
+    @item_priority ||= all_item_types.each_with_index.map { |item, i| {"#{item}": i + 1}}.reduce({}, :merge)
+  end
+
+  def all_item_types
+    @all_item_types ||= ('a'..'z').to_a + ('A'..'Z').to_a
+  end
+
+  def item_types_in_both_compartments
+    first_compartment.uniq & second_compartment.uniq
   end
 end
--- day_three/spec/rucksack_spec.rb
+++ day_three/spec/rucksack_spec.rb
@@ -37,4 +37,58 @@ RSpec.describe Rucksack do
       expect(last_item_list.item_count).to eq 24
     end
   end
+
+  describe ".item_priority" do
+    let(:expected_hash) do
+      {
+        a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7,
+        h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14,
+        o: 15, p: 16, q: 17, r: 18, s: 19 , t: 20, u: 21,
+        v: 22, w: 23, x: 24, y: 25, z: 26,
+        A: 27, B: 28, C: 29, D: 30, E: 31, F: 32, G: 33,
+        H: 34, I: 35, J: 36, K: 37, L: 38, M: 39, N: 40,
+        O: 41, P: 42, Q: 43, R: 44, S: 45, T: 46, U: 47,
+        V: 48, W: 49, X: 50, Y: 51, Z: 52
+      }
+    end
+
+    it "returns the expected hash" do
+      expect(Rucksack.new(items.first).item_priority).to eq expected_hash
+    end
+  end
+
+  describe ".all_item_types" do
+    let(:expected_array) do
+      [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+        'V', 'W', 'X', 'Y', 'Z'
+      ]
+    end
+
+    it "returns the expected hash" do
+      expect(Rucksack.new(items.first).all_item_types).to eq expected_array
+    end
+  end
+
+  describe "#items_in_both_compartments" do
+    it "returns an array of item types found in both compartments" do
+      rucksack_with_dup_item_types =  Rucksack.new(items.first)
+      expect(rucksack_with_dup_item_types.item_types_in_both_compartments).to eq ['p']
+    end
+
+    it "returns an empty array when item types not found in both compartments" do
+      item_list = "ttgJtRGJQcxTZsZT"
+      rucksack_without_dup_item_types =  Rucksack.new(item_list)
+      expect(rucksack_without_dup_item_types.item_types_in_both_compartments).to be_empty
+    end
+  end
+
+  describe "#sum_of_item_priorties" do
+  end
 end
